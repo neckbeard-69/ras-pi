0bdb141c1b443db57d6de63f06f791b1a0217560
neckbeard-69 <mohammedredha908@gmail.com>
2024-12-06 11:01:38 +0300

    buzz added

--- main.py
+++ main.py
@@ -10,16 +10,29 @@ TEMP_HUMIDITY_PIN = 23
 PIR_PIN = 12
 TRIGGER_PIN = 5
 ECHO_PIN = 6
+BUZZ_PIN = 22
 
 GPIO.setup(GAS_PIN, GPIO.IN)
 GPIO.setup(PIR_PIN, GPIO.IN)
 GPIO.setup(ECHO_PIN, GPIO.IN)
 GPIO.setup(TRIGGER_PIN, GPIO.OUT)
+GPIO.setup(BUZZ_PIN, GPIO.OUT)
+GAS_FREQ = 261
+DISTANCE_FREQ = 196
+MOTION_FREQ = 220
+TEMP_FREQ = 247
+HUMIDITY_FREQ = 300
+def buzz(frequency):
+    pwm = GPIO.PWM(BUZZ_PIN, frequency)
+    pwm.start(50)
+    time.sleep(1)
+    pwm.stop()
 
 def detect_motion():
     pir_value = GPIO.input(PIR_PIN)
     if pir_value == GPIO.HIGH:
         print("motion detected")
+        buzz(MOTION_FREQ)
         return
     print("no motion detected")
 
@@ -28,14 +41,17 @@ def detect_temp_humidity():
 
     if  temp >= 50:
         print("temp is greator than 50")
+        buzz(TEMP_FREQ)
     if humidity is not None:
         print("himidity is detected")
+        buzz(HUMIDITY_FREQ)
     print("Failed to retrieve data from the DHT11 sensor.")
 
 def detect_gas():
     gas_present = GPIO.input(GAS_PIN)
     if gas_present == GPIO.LOW:
         print("gas detected")
+        buzz(GAS_FREQ)
         return
     print("no gas detected")
 
@@ -53,6 +69,7 @@ def detect_distance():
     distance = pulse_duration * 34300 / 2
     print(f"Distance: {distance:.2f} cm")
     if distance > 30:
+        buzz(DISTANCE_FREQ)
         print("High distance")
 
 try:
