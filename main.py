f81dadcfcc504d37d4a5296fa4e45ec8947d0227
neckbeard-69 <mohammedredha908@gmail.com>
2024-12-06 09:34:13 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-
 import RPi.GPIO as GPIO
+import Adafruit_DHT
 import time
 
-GPIO.setmode(GPIO.BCM)
 
-DO_PIN = 18 
-GPIO.setup(DO_PIN, GPIO.IN)
+GPIO.setmode(GPIO.BCM)
+sensor = Adafruit_DHT.DHT11
+GAS_PIN = 18 
+TEMP_HUMIDITY_PIN = 23
+GPIO.setup(GAS_PIN, GPIO.IN)
 
 try:
     while True:
-        gas_present = GPIO.input(DO_PIN)
-
+        gas_present = GPIO.input(GAS_PIN)
+        humidity, temp = Adafruit_DHT.read_retry(sensor, TEMP_HUMIDITY_PIN)
+        if humidity is not None and temp is not None:
+            print(f"Temperature: {temp:.1f} °C, Humidity: {humidity:.1f} %")
+        else:
+            print("Failed to retrieve data from the DHT11 sensor.")
         if gas_present == GPIO.LOW:
             gas_state = "Gas Present"
         else:
@@ -18,7 +24,7 @@ try:
 
         print(f"Gas State: {gas_state}")
 
-        time.sleep(0.5)  
+        time.sleep(2)  
 
 except KeyboardInterrupt:
     print("Gas detection stopped by user")
